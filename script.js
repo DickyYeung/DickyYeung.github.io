fc5c5d6ff0b5f4e7b6695e77dc1cf749ea1877ca
Dicky <91754616+DickyYeung@users.noreply.github.com>
2025-01-06 23:35:15 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// script.js
 function displayCurrentTime() {
     var currentTime = new Date();
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
   
-    // 如果時、分、秒少於10,則在前面加上0
     hours = (hours < 10 ? "0" : "") + hours;
     minutes = (minutes < 10 ? "0" : "") + minutes;
     seconds = (seconds < 10 ? "0" : "") + seconds;
@@ -13,6 +11,6 @@ function displayCurrentTime() {
     var timeString = hours + ":" + minutes + ":" + seconds;
     document.getElementById("time-display").textContent = timeString;
   }
-  
-  // 每隔1秒更新一次時間
+
   setInterval(displayCurrentTime, 1000);
+  
